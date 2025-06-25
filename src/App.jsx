04251714396af6fd7e5153dfcf7b8e2fa08a3c5b
oktab <aslbekxayrullayev1@gmail.com>
2025-06-25 09:18:53 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Home from './Components/Home/Home.jsx';
+import Book from './Components/Book/Book.jsx';
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
