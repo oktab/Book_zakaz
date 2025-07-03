798b6df0123622507d03a6c3498c4f8f32215fd7
Stilla <sunnatilla95@gmail.com>
2025-07-03 09:59:04 +0500

    api ulandi books

--- src/Components/Loction/Location.jsx
+++ src/Components/Loction/Location.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import location from './../../assets/img/location.png';
+import { IMaskInput } from "react-imask";
 function Location() {
     const { t } = useTranslation();
+
+    const [phone, setPhone] = useState("")
 
     return (
         <div className="max-w-[1177px] w-full h-[481px] mx-auto">
@@ -12,10 +15,10 @@ function Location() {
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11968.210470783546!2d69.29967315!3d41.41637645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef323741fc927%3A0xd4c294debb101a51!2sBO&#39;YIN.UZ%20CHOYHONA!5e0!3m2!1sru!2s!4v1751094645071!5m2!1sru!2s"
                         width="600"
                         height="650"
-                        style={{ border: 0 }}             
-                        allowFullScreen                      
+                        style={{ border: 0 }}
+                        allowFullScreen
                         loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade" 
+                        referrerPolicy="no-referrer-when-downgrade"
                     ></iframe>
                     <h1 className="absolute top-4 left-4 text-3xl font-bold text-black font-inte px-2 py-1">
                         {t('location_title')}
@@ -32,16 +35,28 @@ function Location() {
                             type="text"
                             className="bg-white rounded-md pl-[10px] w-[444px] h-[47px] mt-[20px]"
                             placeholder={t('location_form_name')}
-                        /> <br />
-                        <input
+                        />
+
+                        <IMaskInput
+                            mask="+998 (00) 000-00-00"
+                            value={phone}
+                            onAccept={(value) => setPhone(value)}
+                            placeholder="+998 (__) ___-__-__"
+                            className="bg-white rounded-md pl-[10px] w-[444px] h-[47px] mt-[10px]"
+                        />
+
+
+                        {/* <input
                             type="number"
                             className="bg-white rounded-md pl-[10px] w-[444px] h-[47px] mt-[10px]"
-                            placeholder={t('location_form_phone')}
-                        /> <br />
+
+                        /> */}
+
+                        {/*  placeholder={t('location_form_phone')} */}
                         <textarea
                             className="bg-white resize-none rounded-md pl-[10px] w-[444px] h-[77px] mt-[10px]"
                             placeholder={t('location_form_question')}
-                        ></textarea> <br />
+                        ></textarea>
                         <button className="text-white bg-[#098C81] w-[444px] h-[47px] rounded-md mt-[10px]">
                             {t('location_form_button')}
                         </button>
--- src/Pages/Book/Books.jsx
+++ src/Pages/Book/Books.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import BooksSection from './BooksSection'
 
 const Books = () => {
+
   return (
     <>
       <BooksSection />
