ac7fb8055c87ed65dd13cbe74b2437559e2c9301
oktab <aslbekxayrullayev1@gmail.com>
2025-08-11 14:42:40 +0300

    boldi saralsh ishladi faqat react-spinners kutubxonasini sihlatdim load uchun

--- src/i18n.js
+++ src/i18n.js
@@ -5,17 +5,28 @@ import translationUZ from './locales/uz/translation.json';
 import translationRU from './locales/ru/translation.json';
 import translationEN from './locales/en/translation.json';
 
+const resources = {
+  uz: { translation: translationUZ },
+  ru: { translation: translationRU },
+  en: { translation: translationEN },
+};
+
 i18n
   .use(initReactI18next)
   .init({
-    resources: {
-      uz: { translation: translationUZ },
-      ru: { translation: translationRU },
-      en: { translation: translationEN },
-    },
+    resources,
     lng: localStorage.getItem('i18nextLng') || 'uz',
     fallbackLng: 'uz',
     interpolation: { escapeValue: false },
+    detection: {
+      order: ['localStorage', 'navigator'],
+    },
   });
+i18n.on('languageChanged', (lng) => {
+  localStorage.setItem('i18nextLng', lng);
+});
+export const getApiLanguage = () => {
+  return (i18n.language || 'uz').toUpperCase();
+};
 
 export default i18n;
